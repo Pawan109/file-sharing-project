d86a79193b551a878fd22067ee4327651a786639
Pawan109 <68591381+Pawan109@users.noreply.github.com>
2023-02-02 09:37:24 +0530

    Update file.js

--- models/file.js
+++ models/file.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+//schema
 const fileSchema = new Schema({
     filename: { type: String, required: true },
     path: { type: String, required: true },
